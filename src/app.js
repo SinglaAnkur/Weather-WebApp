cce9ae9157e2dd60f4601f61bcda6905d65bdb5c
unknown <singla.ankur147@gmail.com>
2020-09-09 18:22:03 -0400

    Serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 const express = require('express')
+const path = require('path')
 const { response } = require('express')
 
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, "../public")
+
+app.use(express.static(publicDirectoryPath))
 app.get('',(req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Hello express!</h1>')
 })
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
+// app.get('/help', (req, res) => {
+//     res.send({
+//         name: 'Ankur',
+//         Place: 'Montreal'
+//     })
+// })
 
-app.get('/about', (req, res) => {
-    res.send('About page')
-})
+// app.get('/about', (req, res) => {
+//     res.send('About page')
+// })
 
 app.get('/weather', (req, res) => {
     res.send('Weather page')
