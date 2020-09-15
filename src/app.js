66884cbdd4eee3eb9ae4a52fc1bdbd7a2fc8f460
unknown <singla.ankur147@gmail.com>
2020-09-15 18:03:04 -0400

    Rendering real time weather data from weather stack and mapbox api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const { response } = require('express')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -32,6 +34,30 @@ app.get('/about', (req, res) => {
     })
 })
 
+app.get('/weather', (req,res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude,longitude,location}) => {
+        if(error) {
+            return res.send({error})
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address: req.query.address
+            })
+          })
+    })
+})
 app.get('/help', (req, res) => {
     res.render('help', {
         helpText: "Some Text",
